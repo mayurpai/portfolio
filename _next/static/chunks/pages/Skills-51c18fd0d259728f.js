792b9c2d39bfcbec9b497e504e1f0e9da02f98c2
mayurpai <mayur5pai@gmail.com>
2022-07-14 13:23:28 +0000

    Deploying to gh-pages from @ mayurpai/portfolio@e4c9e41e02946fb0583f86823fdba3fb5f43b79a 🚀

---
+++ _next/static/chunks/pages/Skills-51c18fd0d259728f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[462],{464:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Skills",function(){return u(1050)}])}},function(n){n.O(0,[247,638,50,774,888,179],(function(){return _=464,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
