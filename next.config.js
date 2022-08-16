506d9b3af7f8703e785f7420f7f741904e5a6a55
mayurpai <mayur5pai@gmail.com>
2022-08-16 21:25:23 +0530

    Crap

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    loader: "akamai",
-    path: "",
-  },
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio",
+  // images: {
+  //   loader: "akamai",
+  //   path: "",
+  // },
+  // basePath: "/portfolio",
+  // assetPrefix: "/portfolio",
   // Comment Lines images To assetPrefix To Run It Locally (Icon Won't Be Visible, That's Okay!) 
 };
 
