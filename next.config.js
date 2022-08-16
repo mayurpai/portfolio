00397067a2ff9343c397f5f03632c79a438bd69f
mayurpai <mayur5pai@gmail.com>
2022-08-16 21:44:58 +0530

    Wasted Time

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // images: {
-  //   loader: "akamai",
-  //   path: "",
-  // },
-  // basePath: "/portfolio",
-  // assetPrefix: "/portfolio",
-  // Comment Lines images To assetPrefix To Run It Locally (Icon Won't Be Visible, That's Okay!) 
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio",
+  // Comment Lines images To assetPrefix To Run It Locally (Icon Won't Be Visible, That's Okay!)
 };
 
 module.exports = nextConfig;
