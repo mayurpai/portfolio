311735a2d1641e10aa60b248f101e76cee83f52b
mayurpai <mayur5pai@gmail.com>
2023-11-08 00:57:56 +0530

    First Poetry Added

--- src/constants/Poetry.jsx
+++ src/constants/Poetry.jsx
@@ -4,4 +4,6 @@ const magicianOfWords = "Magician Of Words";
 const lajzonKaJadugar = "Lajzon Ka Jadugar";
 const link = Social.LajzonKaJadugar;
 
+const status = "जादू हैं इन लफ़्ज़ों मैं, आपको प्यार ना हो जाए हमसे !";
+
 export const Poetry = [{}];
