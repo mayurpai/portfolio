b58ba8fa5882edc2d20f92a361d04e51355cb703
mayurpai <mayur5pai@gmail.com>
2023-12-13 00:08:59 +0530

    Adding More Poetry

--- src/constants/Poetry.jsx
+++ src/constants/Poetry.jsx
@@ -129,6 +129,490 @@ const Poetry = [
     date: "October 27, 2022",
     category: category[0],
   },
+  {
+    description: [
+      "Na Jaane Kithno Ne Humein Bala Bura Kaha Ho", 
+      "Ki, Na Jaane Kithne Log Aise Hai, Jo Dusron Ki Baathon Mai Aakar Humein Bura Samajhte Ho",
+      "Janab, Burayi Tum Unhi Logon Ki Karthe Ho. Jinse Tum Barabari Nahi Kar Pathe Ho",
+    ],
+    date: "July 29, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "As Time Flies, People Change. You're Just A Medium Of Exchange.", 
+      "As Time Flies, People Change. They Tend To Behave Strange.",
+    ],
+    date: "July 27, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Kuch Logh Kithne Ajeeb Hothe Hai", 
+      "Aapko Tabhi Yaadh Karthe Hai, Jab Unhe Aapki Zarurat Hothi Hai",
+      "Na Jane Kyu Hai Woh Aise, Ek Baar Hi Sahi, Unhein Bina Kisi Matlab Ke Yaadh Karke Toh Dekhiye",
+    ],
+    date: "July 21, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "वक्त Ke Saath लोग बदलते Hai, जैसे चाहे Waise बदलते Hai", 
+      "Ke, समय Ke Saath सभी बदलते Hai, Jab ज़रूरत Pade Tab बदलते Hai",
+      "जिंदगी Mai Kisi Aise Ko ढूंढो, Ki वक्त बदले Ya ज़माना, Na छोड़े Kabhi साथ तुम्हारा।",
+    ],
+    date: "April 06, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Jab मैं छोटा ता माँ Ke Saamne रोने Se Jo माँगा Woh मिलता", 
+      "Bohoth बार उन्हें रोते Dekha पर Hamesha सवाल Yeh Raha Ki मुझसे Kyun Kabhi कुछ नहीं माँगा?",
+      "Jab मैं छोटा ता माँ Ke Saamne रोने Se Jo माँगा Woh मिलता",
+      "आज Jab मैंने Unka सपना पूरा Kiya तो उन्हें रोते Dekha",
+    ],
+    date: "April 05, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "कान खोल कर Sunnlo जलने वालों", 
+      "Haaji, कान खोल कर Sunnlo जलने वालों",
+      "हमसे बराबरी Nahi Kar पाओगे Yeh गलत फहमी Apne दिमाग Se Pehle निकालो",
+    ],
+    date: "April 05, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "आपकी फ़रमाइश Par, हमने Chuna हयात", 
+      "Sirf सामने Se देते Hai हाथ पीट पीछे खंजर Marna Hai Inki औकात",
+      "हमारी Hogi देर सारे लोगों Se मुलाक़ात Kuch Se रिश्तें नाते Rahenge विश्वासघात",
+      "नाकामयाबी Ka दोष Denge Apne हालात Jab Tak Na बने विख्यात",
+      "बिल्कुल अच्छी Nahi Hai Yeh देहात हमारी Koi पूछता Nahi Hai खैरियत",
+      "Hogi Kuch परेशानियाँ Iss रात क्या Patha कल Ho सुप्रभात",
+      "Ek दूसरे Ke दो साथ Kijiye अपनो Se Kuch बात यही Toh Hai हयात",
+    ],
+    date: "April 04, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Your World Will Haue Thousands Like Me, But Mine Will Not Have Anyone Like You!", 
+    ],
+    date: "April 04, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Jaise 'एक सिक्के के दो पहलू होते हैं।'", 
+      "Ussi Tarah, 'दोस्ती के दो पहलू होते हैं।'",
+      "Ek आपकी मदद Karne Ke Liye एक तरीका ढुंढेगा Aurr Dusra आपकी मदद Na Karne Ke बहाने ढुंढेगा!",
+    ],
+    date: "April 04, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "खुदा से मांगी हुई मन्नाथ है तू", 
+      "खुदा की कसम मेरी जन्नत है तू।",
+    ],
+    date: "April 04, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "नशा Tah तेरे Iss प्यार Mai, Jisme हम खो गए", 
+      "इश्क़ Ka समुन्दर Tah, Jisme हम डूब गए",
+      "हमें Bhi Nahi Patha Chala, Kab हम तेरे हो गए",
+    ],
+    date: "April 03, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "I Just Wanna Say", 
+      "I Crave For You Everyday",
+    ],
+    date: "April 12, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "प्यार Unse Karo Jo आपके मुस्कुराने Ki वजह हैं,", 
+      "Unse Karo Jo आपके ख़ुशी Ka एहसास हैं,",
+      "Unse Karo Jo आपके Liye बहुत खास",
+      "Kya आप Hi Hai वो, Jiski मुझे तलाश हैं?",
+    ],
+    date: "April 03, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Never Let Anyone Get So Close To You That You Dig Your Own Grave", 
+    ],
+    date: "April 11, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Jaise गिरगिट Apna रंग और रूप, समय Aane Par परिवर्तन Karthe Hai", 
+      "मेरा Aise लोगों Se नाता जड़ा Hai, जो मूड और ज़रूरत Aane Par बदलते Hai", 
+    ],
+    date: "April 11, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "If Someone Is Your Addiction", 
+      "You Better Be Ready For Affliction!", 
+    ],
+    date: "April 10, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Every Friendship Starts New, Only Some Of Them Turn True", 
+      "The People Who Make An Effort To Be With You, Exist In Numbers Very Few", 
+      "These Are Made Without Any Interview, Neither Includes Revenue", 
+      "Everything Seems Difficult To Get Through, Until Your Friend Finds You A Clue", 
+      "Together, We Can Face Any Issue No Matter What, We'll Just Screw", 
+      "Our Friendship Is The Fricking Breakthrough!", 
+    ],
+    date: "April 10, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Yeh दुनिया Hai हमसे न्यारा", 
+      "कौन Hai Kiska सहारा", 
+      "Aage बहुत Hai परेशानियाँ", 
+      "इस्तेमाल Kare Apni शक्तियां", 
+      "सबकी Hai एक Na एक कमज़ोरिया", 
+      "जैसे Har एक Ki Hai परछाईयाँ", 
+      "आसान Nahi Hai चुनौतियां", 
+      "Rahenge बहुत कठिनाइयां", 
+      "हार Kabhi Na मानोगे, Karo अपने आपसे वादा", 
+      "आंधी हो या तूफान डटे रहना Hai इरादा", 
+    ],
+    date: "April 09, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "जाने अनजाने मैं Agar Maine Kisiko ठेस पहुँचाया हैं Toh माफ़ी", 
+      "मेरा Waisa कोई इरादा Nahi ता तथापि", 
+    ],
+    date: "April 08, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "लोग Mera Kal देखते Hai, हम Unka आज Dekthe Hai", 
+      "लोग Mera उपयोग Kaise Kare देखते Hai, हम Unki सहायता Kaise Kare देखते Hai", 
+      "Bas अंतर यह Hai Ki, लोग Mujhe लाख में से एक Dekthe Hai, हम Unhe लाखों में एक Dekthe Hai", 
+    ],
+    date: "April 08, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Mere Kuch अल्फ़ाज़ आपके Liye, Agar Hai आपकी इजाज़त", 
+      "आपके रूप Mai भगवान Ne दी Mujhe बरकत", 
+      "Aise कहना Nahi Hai गलत, चाँद Jithni Hai आप खुबसूरत, Jiske साथ Mai Karu शरारत", 
+      "किसने सोचा Tah, आपको Paana Hai मेरी किस्मत, आपका Meri जिंदगी Mai आना Hai हैरत, बन चुकी Hai आप Hamari ज़रूरत, जिसकी Mai Karu हिफाज़त", 
+	  "मुझे नहीं Chahiye Kisi Ki दौलत, Chahiye Toh बस Kuch Aurr मोहलत, आपको बनाना Hai हमारी राहत, Kyu Ki आप Hai हमारी चाहत",
+	  "Mere दिल का सुकून Hai आपकी मुस्कराहट, आप Hi Toh Hai हमारी ताकत, आप Jaisa कोई नहीं Dekha Humne शख्सियत, करेंगे दोनो Ek Dusre Se प्यार Jaise Koi हसरत",
+	  "बन Jo चुकी Hai आप Meri Ab आदत, बोल Hi Dijiye, आप Bhi करती HaiHumse मोहबत, Kya आप Hai Isse सहमत?",
+	  "Kaise Lagi आपको हमारी लिखावट?"
+    ],
+    date: "April 07, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Jis Insaan Ke Liye Humne Sab Chhodkar Apna Pura Samay Diya", 
+      "Aaj Ussi Insaan Ne Humein Busy Bolkar Kisi Aurr Ko Apna Samay Diya", 
+    ],
+    date: "April 19, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Don't Ever Change Your Life For One Person", 
+      "That Change Will Hurt You, As You See The Person Change", 
+    ],
+    date: "April 18, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "It Is Disappointing When You Put So Much Effort Into Something, Only To Have People Ignore It In The End.", 
+    ],
+    date: "April 17, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "अब रिश्तें Kuch Aise Hai, Na पहले Jaise बातें होती Hai, Naahi अता-पता Hai", 
+    ],
+    date: "April 17, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "मेरी दिल Ki धड़कन Hai Tu, मेरी दिल Ka सुकून Hai Tu,", 
+      "मेरी चाहत Hai Tu, मेरी ज़रूरत Hai Tu,", 
+      "Jaise Bhi Ho, मेरी जान Hai Tu!", 
+    ],
+    date: "April 16, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "तुम क्या जानो, क्या हाल हुआ हैं हमारा", 
+      "जबसे तुझे देखा हैं, एक ही सवाल हैं हमारा", 
+      "क्यों हर पल सिर्फ और सिर्फ ख़याल हैं तुम्हारा?", 
+    ],
+    date: "April 15, 2022",
+    category: category[1],
+  },
+  {
+    description: [
+      "आज Ke दिन Aise लोग कम Milthe Hai", 
+      "जो Aapko, बिना Judge Kiye आपसे बात Karthe Hai", 
+    ],
+    date: "April 15, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Ever Thought, How Beautiful Is Our Friendship", 
+      "When I'm With You, World Seems To Be At My Fingertip", 
+      "It's Never Gonna Be Easy For Someone To Break This Partnership", 
+      "A Life Without You Would Just Seem To Sadly Drip", 
+      "Together We Are Smart As A Whip Perhaps Alone, Will Never Achieve A Scholarship", 
+      "Unitedly, We Can Goddam Win Any Championship", 
+      "Now That's What I Call A Friendship!", 
+    ],
+    date: "April 12, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Uska Muskurana, Mera Sukoon Hai", 
+      "Usko Paana, Mera Junoon Hai", 
+      "Duaon Main, Agar Kisi Ko Maanga Hai,", 
+      "Toh Woh Sirf Aurr Sirf Tu Hai", 
+    ],
+    date: "April 26, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Life Is A Journey, That Is Unjust", 
+      "People Might Make You Feel Disgust", 
+      "There Won't Be Anyone To Trust", 
+      "You Will Have To Adjust", 
+      "Having Positive Mindset Is A Must", 
+    ],
+    date: "April 25, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Maana Ki, Waqt Hamare Saath Nahi Raha", 
+      "Maana Ki, Hamara Naseeb Kuch Khaas Nahi Raha", 
+      "Magar Aap Loghon Ka Pyaar Dekhkar, Ab Mai Udaas Nahi Raha!", 
+    ],
+    date: "April 24, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Trust Is Not Something You Can See", 
+      "However, When Broken, Even The Blind Can See!", 
+    ],
+    date: "April 24, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Kehna Unka Maano Jo Aapke Liye Hamesha Sahi Chahthe Hai", 
+      "Yakeen Mano, Parivaar Ke Alawa Aurr Koi Nahi Hai", 
+    ],
+    date: "April 25, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Your Life Is Yours To Keep Whatever You Want", 
+      "However, There Is One Thing You Should Not Keep ...",
+	  "EXPECTATIONS!"
+    ],
+    date: "April 22, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "कहते Hain, अपने Dil Ki Suno", 
+      "मेरा अपनो Se भरोसा उठ Chuka Hain",
+	  "Dil हजार हिस्सों में टूट Chuka Hain",
+	  "Bolo, किस हिस्से Ka सुनना Hain?"
+    ],
+    date: "April 21, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "The World Is A Strange Place. Lies Do Not Break Relationships Here, But Telling The Truth Does."
+    ],
+    date: "May 06, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "He: How Do You Always Manage To Do This?",
+      "She: Do What?",
+      "He: Melt My Heart Like This"
+    ],
+    date: "May 05, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "We Live In A Generation That Expects A Lot From Others.",
+      "But Tell, I Don't Expect Anything From Others."
+    ],
+    date: "May 04, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "From Waiting For Each Other To Come Online.",
+      "To The Message Wasn't Read, Even When Online.",
+	  "It Has Been A Journey That Faded Over Time Due To Miscommunications And Misunderstandings."
+    ],
+    date: "May 02, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Those Who Teach You, Not To Need Anyone, Will Be Those You Need The Most."
+    ],
+    date: "April 29, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Kisine Humse Pucha, Tera \"Mol\" Kya Hai?",
+	  "Maine Bola, Meri Har Ek Bimaari Ki Woh \"Paracetamol\" Hai"
+    ],
+    date: "April 29, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Maine Kismat Se, Zyada Bharosa Tumpe Kiya Tah",
+	  "Lekin, Kismat Zyada Nahi Badla Jithna Tu Badli"
+    ],
+    date: "April 28, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Meri Mehnat Se Jithna Kush Mai Raha, Meri Kismat Ne Mujhe Usse Zyada Rulaaya",
+	  "Meri Har Koshish Naakam Raha, Aaj Mai Kuch Kaam Na Raha"
+    ],
+    date: "May 17, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Meri Sapno Ki Raani, Banja Tu Meri Jeevansathi",
+    ],
+    date: "May 15, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Tum Puch Lena Suraj Se, Yakeen Na Ho Toh Chaandh Se,",
+	  "Mera Dil Dadaktha Hai, Sirf Aurr sirf Tere Naam Se!"
+    ],
+    date: "May 14, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Earlier, It Was Just That You Looked Good.",
+	  "But Now, Apart From You Nobody Else Looks Good."
+    ],
+    date: "May 13, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Yeh Dil Tujhe Ithni Shiddath Se Chahtha Hai",
+	  "Meri Har Dua Mai, Tera Hi Naam Aata Hai"
+    ],
+    date: "May 12, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Kithne Vajan Ka Hoga?",
+	  "Pucho Kya?",
+	  "Tujh Jaise Darthi Par Bhoj Ko Hatana Hoga"
+    ],
+    date: "May 10, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "If You Don't Wish To Keep Us One, Just Say",
+	  "Cuz I Hate People Who Fake, Without Having The Audacity To Say.",
+    ],
+    date: "May 09, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Everybody Is Going Through Some Piece Of Shit.",
+	  "Ironically, Everybody Feels Only They Deal With A Lot Of Shit.",
+    ],
+    date: "May 06, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Surely You Heard It In Love, However, My Friendship Ended Up Being One-sided.",
+    ],
+    date: "May 07, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Aapko Jab Pehli Baar Dekha, Koi Ithna Khoobsurat Ho Sakta Hai, Tab Jaana",
+      "Kasam Se, Khoobsurati Bhi Sharma Gayi, Alse Apko Bhagwaan Ne Banaya",
+    ],
+    date: "May 24, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "The Day, People Understand The Difference Between Need And Want Is The Day, When Feople Will Stop To Flaunt!",
+    ],
+    date: "May 19, 2022",
+    category: category[0],
+  },
+  {
+    description: [
+      "Tumhe Patha Hai, Tu Kithni Khoobsurath Hai?",
+	  "Khoobsurati Bhi Sharma Jaaye, Uthni Khoobsurath Hai"
+    ],
+    date: "May 18, 2022",
+    category: category[0],
+  },
 ];
 
 const sortedPoetry = sortByDate(Poetry);
