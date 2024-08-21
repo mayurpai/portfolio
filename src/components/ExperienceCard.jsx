88ede4d2d7254c0333b052f49760d45272e1e434
Mayur Pai <mayur5pai@gmail.com>
2024-08-22 01:17:28 +0530

    Experience Card Work In Progress

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -1,118 +1,92 @@
-import { Projects } from "@/constants/Project";
+import { Experiences } from "@/constants/Experiences";
+import styles from "@/styles/components/ExperienceCard.module.scss";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import {
-  faCss3Alt,
-  faGithub,
-  faHtml5,
-  faJs,
-  faReact,
+    faCss3Alt,
+    faGithub,
+    faHtml5,
+    faJs,
+    faReact,
 } from "@fortawesome/free-brands-svg-icons";
 import { faCode, faLink } from "@fortawesome/free-solid-svg-icons";
 import { PT_Mono, Sigmar_One } from "next/font/google";
-import Link from "next/link";
-import Tilt from "react-vanilla-tilt";
-import styles from "../styles/components/Card.module.scss";
 import Image from "next/image";
-import Technology from "./Technology";
-import Experience from "@/sections/Experience";
-import { Experiences } from "@/constants/Experiences";
 library.add(faGithub, faLink, faCode, faHtml5, faCss3Alt, faJs, faReact);
 
 const sigmar_One = Sigmar_One({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
+    weight: "400",
+    subsets: ["latin"],
+    display: "swap",
 });
 
 const pt_mono = PT_Mono({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
+    weight: "400",
+    subsets: ["latin"],
+    display: "swap",
 });
 
 const imageStyle = {
-  borderRadius: "1rem",
-  border: "none",
-  padding: "0.25rem",
-  outline: "none",
-  objectFit: "cover",
+    borderRadius: "1rem",
+    border: "none",
+    padding: "0.25rem",
+    outline: "none",
+    objectFit: "cover",
 };
 
 export default function ExperienceCard() {
-  return Experiences.map((item, id) => {
-    let link = item.webLink == "#" ? item.codeLink : item.webLink;
-    const isTitleTooLong = item.title.length > 16;
-    const isTechnologyTooLong = item.technology.length > 3;
-    // console.log(item);
-    return (
-      <div className={styles.projects_card} key={id}>
-        <Link href={link} target="_blank">
-          <Tilt
-            className={styles.tilt}
-            options={{
-              reverse: false,
-              max: 345,
-              perspective: 1000,
-              scale: 5,
-              speed: 300,
-              transition: true,
-              axis: null,
-              reset: false,
-              easing: "cubic-bezier(.03,.98,.52,.99)",
-              glare: false,
-              "max-glare": 1,
-              "glare-prerender": false,
-            }}
-            style={{
-              backgroundColor: "transparent",
-              boxShadow: "none",
-            }}
-          >
-            <div className={styles.card}>
-              <div className={styles.box}>
-                <div className={styles.content}>
-                  <h2 className={`${sigmar_One.className} ${styles.id}`}>
-                    {id + 1}
-                  </h2>
-                  <div className={styles.project_image}>
-                    <Image
-                      style={imageStyle}
-                      fill
-                    //   sizes="100vw"
-                      src={item.imgLink}
-                      alt={item.title}
-                      priority={true}
-                    ></Image>
-                  </div>
-                  <div className={styles.technology_section}>
-                    <Technology names={item.technology} />
-                    <h3 className={`${styles.title}`}>{item.title}</h3>
-                    <h6 className={pt_mono.className}>
-                      {item.fromDate} - {item.toDate}
-                    </h6>
-                    <p
-                      className={`${styles.description} ${pt_mono.className}`}
-                      style={
-                        isTitleTooLong && isTechnologyTooLong
-                          ? {
-                              display: "-webkit-box",
-                              WebkitLineClamp: 3,
-                              WebkitBoxOrient: "vertical",
-                              overflow: "hidden",
-                              textOverflow: "ellipsis",
-                            }
-                          : {}
-                      }
-                    >
-                      {item.shortDescription}
-                    </p>
-                  </div>
+    return Experiences.map((item, id) => {
+        const isDescTooLong = item.description.length > 50;
+        return (
+            <div className={styles.experience_card} key={id}>
+                {/* <Link href={link} target="_blank"> */}
+                <div className={styles.card}>
+                    <div className={styles.box}>
+                        <div className={styles.content}>
+                            <div className={styles.logo}>
+                                <Image
+                                    style={imageStyle}
+                                    fill
+                                    src={item.image}
+                                    alt={item.name}
+                                    priority={true}
+                                ></Image>
+                            </div>
+                            <div className={styles.context_holder}>
+                                <div className={styles.experience_content_section}>
+                                    <h3 className={`${styles.title}`}>{item.name}</h3>
+                                    <h6 className={`${pt_mono.className} ${styles.id}`}>
+                                        {item.role.map((role, index) => {
+                                            const fromIndex = index; // Normal index for the 'from' array
+                                            const toIndex = fromIndex + 1; // Next index in 'from', or 'to' if it's the last role
+                                            return (
+                                                <p key={index}>
+                                                    <span className={styles.role}>{role}</span> <span className={styles.dividor}>|</span> <span className={styles.duration}>{item.from[fromIndex]} - {toIndex < item.from.length ? item.from[toIndex] : item.to}</span>
+                                                </p>
+                                            );
+                                        }).reverse()}
+                                    </h6>
+                                </div>
+                                <p
+                                    className={`${styles.description} ${pt_mono.className}`}
+                                    style={
+                                        isDescTooLong ?
+                                            {
+                                                display: "-webkit-box",
+                                                WebkitLineClamp: 5,
+                                                WebkitBoxOrient: "vertical",
+                                                overflow: "hidden",
+                                                textOverflow: "ellipsis",
+                                            } : {}
+                                    }
+                                >
+                                    {item.description}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </Tilt>
-        </Link>
-      </div>
-    );
-  });
+                {/* </Link> */}
+            </div >
+        );
+    });
 }
--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,17 +1,20 @@
-import styles from "@/styles/sections/Experience.module.scss";
 import BoilerHeading from "@/common/BoilerHeading";
+import ExperienceCard from "@/components/ExperienceCard";
+import styles from "@/styles/sections/Experience.module.scss";
 
 const heading = "Experience";
 
-export default function Experience() {
-  return (
-    <section className={styles.experience_conatiner} id="experience">
-      <div className={styles.experience_body_container}>
-        <BoilerHeading title={heading} header="h2"/>
-        <div className={styles.experience_section_container}>
 
-        </div>
-      </div>
-    </section>
-  );
+
+export default function Experience() {
+    return (
+        <section className={styles.experience_conatiner} id="experience">
+            <div className={styles.experience_body_container}>
+                <BoilerHeading title={heading} header="h2" />
+                <div className={styles.experience_section_container}>
+                    <ExperienceCard />
+                </div>
+            </div>
+        </section>
+    );
 }
