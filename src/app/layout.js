8dbd424edd0ed6c4a9441bb2108df3e5571027cc
Mayur Pai <mayur5pai@gmail.com>
2024-08-23 16:09:33 +0530

    Metadata Updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ config.autoAddCss = false;
 
 export const metadata = {
     title: "Mayur Pai",
-    description: "I am Mayur Pai B H, a web developer, and programmer.",
+    description: "I 'm a web wizard and coding aficionado, always on a quest to conquer the wildest realms of programming. Cracking complex challenges and blazing new trails is my forte. Teaching fuels my passion and keeps my skills razor-sharp and my mind always buzzing. Armed with a Bachelor's in Information Science from Sahyadri College of Engineering and Management, Mangalore, my credentials are as next-level as my tech game. I don't just keep up with trends—I set them! Currently, I'm architecting the next wave of generative AI innovations at GalaxE. Solutions an Endava Company. My love affair with computers ignited early and it's still blazing strong. Equipped with a powerhouse toolkit of languages, algorithms, and leadership flair, I'm here to make waves and build legacies.  There's always more to conquer, more bugs to squash, and more groundbreaking projects to craft. Let's crank up the tech magic to eleven!",
     applicationName: "Portfolio",
     referrer: "origin-when-cross-origin",
     keywords: [
