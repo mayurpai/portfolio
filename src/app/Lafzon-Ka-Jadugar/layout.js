576f55213a0642f1f1c02c4d754b92b6cd66a319
mayurpai <mayur5pai@gmail.com>
2026-02-14 15:53:06 +0530

    Lafzon Ka Jadugar Metadata

---
+++ src/app/Lafzon-Ka-Jadugar/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: "Lafzon Ka Jadugar",
+    description: "Step into a world where words don't just talk—they freaking come alive. A spellbinding collection of verses by Mayur Pai, the Magician Of Words.",
+    keywords: ["poetry", "verses", "Lafzon Ka Jadugar", "Mayur Pai", "creative writing"],
+    creator: "Mayur Pai",
+    author: "Mayur Pai",
+};
+
+export default function Layout({ children }) {
+    return children;
+}
